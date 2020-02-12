080baa85e88e93f62a2014819f67800b0e60386d
ajreid <axr490@miami.edu>
2020-02-12 10:03:40 -0500

    updated code for showing and hiding areas to enter/view notes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,18 @@
+$(document).on("click", ".add-note", function() {
+    let parentID = $(this).parent().parent().attr("article-id");
+    $(this).parent().find("a.view-note").attr("href", "");
+    $(this).parent().find("a.add-note").attr("href", "");
+    $(this).parent().find("a.view-note").css("color", "darkred");
+    
+})
+
 $(document).on("click", ".save-note", function() {
     let parentID = $(this).parent().parent().parent().attr("article-id");
     console.log(parentID);
+
+    $(this).parent().parent().find("a.view-note").attr("href", `#noteview-${parentID}`);
+    $(this).parent().parent().find("a.add-note").attr("href", `#noteadd-${parentID}`);
+    $(this).parent().parent().find("a.view-note").css("color", "darkblue");
 
     // $.ajax({
     //     method: "POST",
